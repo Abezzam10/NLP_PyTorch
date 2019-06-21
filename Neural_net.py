0b0f34bac09c73e2ba8ea8875d439039307212d2
Anirudh Das Bezzam <abezzam@stevens.edu>
2019-06-20 21:11:57 -0400

    Forward_prop_implemenation

---
+++ Neural_net.py
@@ -0,0 +1,42 @@
+import torch.nn as nn
+import torch.nn.functional as F
+
+class Net(nn.Module):
+    def __init__(self):
+        super(Net, self).__init__()
+        self.fc1 = nn.Linear(28 * 28, 200)
+        self.fc2 = nn.Linear(200, 200)
+        self.fc3 = nn.Linear(200, 10)
+
+    def forward(self, x):
+        x = F.relu(self.fc1(x))
+        x = F.relu(self.fc2(x))
+        x = self.fc3(x)
+        return F.log_softmax(x)
+    
+    net = Net()
+    print(net)
+    optimizer = optim.SGD(net.parameters(), lr=learning_rate, momentum=0.9)
+    criterion = nn.NLLLoss()
+    for epoch in range(epochs):
+        for batch_idx, (data, target) in enumerate(train_loader):
+            data, target = Variable(data), Variable(target)
+            # resize data from (batch_size, 1, 28, 28) to (batch_size, 28*28)
+            data = data.view(-1, 28*28)
+            optimizer.zero_grad()
+            net_out = net(data)
+            loss = criterion(net_out, target)
+            loss.backward()
+            optimizer.step()
+            if batch_idx % log_interval == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                        epoch, batch_idx * len(data), len(train_loader.dataset),
+                            100. * batch_idx / len(train_loader), loss.data[0]))
+                net_out = net(data)
+                loss = criterion(net_out, target)
+                loss.backward()
+                optimizer.step()
+            if batch_idx % log_interval == 0:
+                print('Train Epoch: {} [{}/{} ({:.0f}%)]\tLoss: {:.6f}'.format(
+                                epoch, batch_idx * len(data), len(train_loader.dataset),
+                                    100. * batch_idx / len(train_loader), loss.data[0]))
